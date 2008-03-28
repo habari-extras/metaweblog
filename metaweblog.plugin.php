041dcb89b44e533fec9e6d9c6b548800d7b8f155
freakerz <USER@DOMAIN>
2008-03-28 16:25:54 +0000

    Replacing spaces with tabs. Dunno how does got in there, sorry.
    Also starting the work on commenting this plugin.
    
    Also starting the work on podcasting support, hopefully with Angelo Mandato's help! :)
    
    git-svn-id: http://svn.habariproject.org/habari-extras/plugins/metaweblog@94 f4a90942-1786-4aa1-b092-57c31935ddc3

--- metaweblog.plugin.php
+++ metaweblog.plugin.php
@@ -7,7 +7,7 @@ class metaWeblog extends Plugin
 	public function info() {
 		return array(
 			'name' => 'metaWeblog',
-			'version' => '0.8.2',
+			'version' => '0.8.3',
 			'url' => 'http://habariproject.org/',
 			'author' =>	'Habari Community',
 			'authorurl' => 'http://habariproject.org/',
@@ -19,10 +19,10 @@ class metaWeblog extends Plugin
 	
 	public function filter_rsd_api_list( $list ) {
 		$list['MetaWeblog']= array(
-		 'preferred' => 'true',
-		 'apiLink' => URL::get( 'xmlrpc' ),
-		 'blogID' => '1',
-		 );
+			'preferred' => 'true',
+			'apiLink' => URL::get( 'xmlrpc' ),
+			'blogID' => '1',
+		);
 		$list['Atom']['preferred']= 'false';
 		return $list;
 	}
@@ -46,7 +46,7 @@ class metaWeblog extends Plugin
 			}
 			else {
 				$exception = new XMLRPCException(801);
-				$exception->output_fault_xml(); // dies here
+				$exception->output_fault_xml();
 			}
 		}
 	}
@@ -68,13 +68,13 @@ class metaWeblog extends Plugin
 		$struct->userid= $post->author->id;
 		$struct->mt_allow_comments= (isset($post->info->comments_disabled)) ? 0 : 1;
 		$struct->mt_allow_pings= 1;
-		
+
 		return $struct;
 	}
 	
 	public function xmlrpc_metaWeblog__getRecentPosts( $params ) {
 		$user= $this->is_auth( $params[1], $params[2] );
-		
+
 		$posts= Posts::get( array( 'limit' => $params[3], 'status' => Post::status('published') ) );
 		$structCollection= array();
 
@@ -90,16 +90,16 @@ class metaWeblog extends Plugin
 			$struct->userid= $post->author->id;
 			$struct->mt_allow_comments= (isset($post->info->comments_disabled)) ? 0 : 1;
 			$struct->mt_allow_pings= 1;
-			
+
 			$structCollection[]= $struct;
 		}
-		
+
 		return $structCollection;
 	}
 	
 	public function xmlrpc_metaWeblog__getCategories( $params ) {
 		$user= $this->is_auth( $params[1], $params[2] );
-		
+
 		$tags= DB::get_results( 'SELECT * FROM ' . DB::table( 'tags' ) );
 		$structCollection= array();
 
@@ -112,85 +112,97 @@ class metaWeblog extends Plugin
 			$struct->categoryid= $tag->id;
 			$structCollection[]= $struct;
 		}
-		
+
 		return $structCollection;
 	}
 	
 	public function xmlrpc_metaWeblog__editPost( $params ) { 
-	    $user= $this->is_auth( $params[1], $params[2] );
+		$user= $this->is_auth( $params[1], $params[2] );
 
-	    $post= Post::get( array( 'id' => $params[0], 'status' => Post::status( 'any' ) ) );
+		$post= Post::get( array( 'id' => $params[0], 'status' => Post::status( 'any' ) ) );
 
-	    if ( !empty( $post ) ) { 
-	      $post->title= $params[3]->title;
-	      $post->slug= $params[3]->title;
-	      if ( isset($params[3]->categories) ) { 
-	        $post->tags= implode( ',', $params[3]->categories );
-	      }
-	      $post->content= $params[3]->description;
-	      $post->content_type= Post::type('entry');
-	      $post->status= ( $params[4] ) ? Post::status('published') : Post::status('draft');
-	      $post->updated= date( 'Y-m-d H:i:s' );
-	      $post->update();
-	      return true;
-	    }
-	    else {
-	      $exception = new XMLRPCException(806);
-	      $exception->output_fault_xml(); // dies here
-	    }
-	  }
+		if ( !empty( $post ) ) { 
+			$post->title= $params[3]->title;
+			$post->slug= $params[3]->title;
+			
+			if ( isset($params[3]->categories) ) { 
+				$post->tags= implode( ',', $params[3]->categories );
+			}
+			
+			if ( isset($params[3]->enclosure) ) {
 
-	  public function xmlrpc_metaWeblog__newPost( $params ) { 
-	    $user= $this->is_auth( $params[1], $params[2] );
+			}
+			
+			$post->content= $params[3]->description;
+			$post->content_type= Post::type('entry');
+			$post->status= ( $params[4] ) ? Post::status('published') : Post::status('draft');
+			$post->updated= date( 'Y-m-d H:i:s' );
+			$post->update();
+			return true;
+		}
+		else {
+			$exception = new XMLRPCException(806);
+			$exception->output_fault_xml();
+		}
+	}
 
-	    $postdata= array(
-	      'slug' => $params[3]->title,
-	      'title' => $params[3]->title,
-	      'content' => $params[3]->description,
-	      'user_id' => User::identify()->id,
-	      'pubdate' => date( 'Y-m-d H:i:s' ),
-	      'status' => ( $params[4] ) ? Post::status('published') : Post::status('draft'),
-	      'content_type' => Post::type('entry'),
-	    );  
-	    if ( isset($params[3]->categories) ) { 
-	      $postdata['tags']= implode( ',', $params[3]->categories );
-	    }   
-	    $post= Post::create( $postdata );
+	public function xmlrpc_metaWeblog__newPost( $params ) { 
+		$user= $this->is_auth( $params[1], $params[2] );
 
-	    $struct= new XMLRPCStruct();
-	    $struct->postid= $post->id;
+		$postdata= array(
+			'slug' => $params[3]->title,
+			'title' => $params[3]->title,
+			'content' => $params[3]->description,
+			'user_id' => User::identify()->id,
+			'pubdate' => date( 'Y-m-d H:i:s' ),
+			'status' => ( $params[4] ) ? Post::status('published') : Post::status('draft'),
+			'content_type' => Post::type('entry'),
+		);
+		
+		if ( isset($params[3]->categories) ) { 
+			$postdata['tags']= implode( ',', $params[3]->categories );
+		}
+		
+		if ( isset($params[3]->enclosure) ) {
 
-	    return $struct;
-	  }
+		}
+		
+		$post= Post::create( $postdata );
+
+		$struct= new XMLRPCStruct();
+		$struct->postid= $post->id;
+
+		return $struct;
+	}
 
 	public function xmlrpc_blogger__deletePost( $params ) {
 		$user= $this->is_auth( $params[2], $params[3] );
-		
+
 		$post= Post::get( array( 'id' => $params[1] ) );
-		
+
 		if ( !empty( $post ) ) {
 			$post->delete();
 			return true;
 		}
 		else {
 			$exception = new XMLRPCException(806);
-			$exception->output_fault_xml(); // dies here
+			$exception->output_fault_xml();
 		}
 	}
 
 	public function xmlrpc_blogger__getUsersBlogs( $params ) {
 		$user= $this->is_auth( $params[1], $params[2] );
-		
+
 		$struct= array(
-		 new XMLRPCStruct(
-			 array(
-		  'url' => Site::get_url( 'habari' ),
-		  'blogid' => '1',
-		  'blogName' => Options::get( 'title' ),
-			 )
-			)
-		);
-		
+			new XMLRPCStruct(
+					array(
+						'url' => Site::get_url( 'habari' ),
+						'blogid' => '1',
+						'blogName' => Options::get( 'title' ),
+					)
+				)
+			);
+
 		return $struct;
 	}
 	
