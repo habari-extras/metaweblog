429ead8be669843feb14dc8a3f614e728398df64
freakerz <USER@DOMAIN>
2008-09-28 03:13:37 +0000

    Adding newMediaObject support. FormUI to configure where uploads go, use Habari Media Silo's by default. More to come! Also added more error codes - based on NucleusCMS
    git-svn-id: http://svn.habariproject.org/habari-extras/plugins/metaweblog/trunk@1059 f4a90942-1786-4aa1-b092-57c31935ddc3

--- metaweblog.plugin.php
+++ metaweblog.plugin.php
@@ -3,11 +3,13 @@ class metaWeblog extends Plugin
 {
 
 	public $user = NULL;
+	public $upload_dir = NULL;
+	public $upload_url = NULL;
 
 	public function info() {
 		return array(
 			'name' => 'metaWeblog',
-			'version' => '0.8.5',
+			'version' => '0.9',
 			'url' => 'http://habariproject.org/',
 			'author' =>	'Habari Community',
 			'authorurl' => 'http://habariproject.org/',
@@ -15,6 +17,117 @@ class metaWeblog extends Plugin
 			'description' => 'Adds support metaWeblog methods to the XML-RPC server.',
 			'copyright' => '2007'
 		);
+	}
+	
+	/**
+	 * This should be improved, better error messages, more codes
+	 */
+	function filter_xmlrpcexception_get_message($default, $code) {
+		switch ($code) {
+			case 801:
+				return _t( 'Login Error (probably bad username/password combination)' );
+			case 802:
+				return _t( 'No Such Blog' );
+			case 803:
+				return _t( 'Not a Team Member' );
+			case 804:
+				return _t( 'Cannot add Empty Items' );
+			case 805:
+				return _t( 'Amount parameter must be in range 1..20' ); // getRecentItems
+			case 806:
+				return _t( 'No Such Item' );
+			case 807:
+				return _t( 'Not Allowed to Alter Item' );
+			case 808:
+				return _t( 'Invalid media type' );
+			case 809:
+				return _t( 'File is too large (max. upload filesize)' );
+			case 810:
+				return _t( 'Other error on newMediaObject' );
+			default:
+				return $default;
+		}
+	}
+	
+	function action_init() {
+		$this->upload_dir= Options::get('metaweblog__upload_dir');
+		$this->upload_url= Options::get('metaweblog__upload_url');
+	}
+	
+	function action_plugin_activation( $file ) {
+		if( Plugins::id_from_file($file) == Plugins::id_from_file(__FILE__) ) {
+			$this->default_paths();
+			if (!$this->check_files()) {
+				Session::error( _t('Failed to create the upload directory for metaWeblog.') );
+			}
+		}
+	}
+
+	function action_plugin_deactivation( $file ) {
+		if( Plugins::id_from_file($file) == Plugins::id_from_file(__FILE__) ) {
+		}
+	}
+	
+	public function default_paths($force= false) {
+		if ($force | empty($this->upload_dir) | empty($this->upload_url)) {
+			$user_path = HABARI_PATH . '/' . Site::get_path('user', true);
+			Options::set('metaweblog__upload_dir', $user_path . 'files' );
+			Options::set('metaweblog__upload_url', Site::get_url('user', true) . 'files' );
+		}
+	}
+	
+	public function check_files() {
+		if ( !is_dir( $this->upload_dir ) ) {
+			if ( is_writable( dirname( rtrim($this->upload_dir,"\/") ) ) ) {
+				mkdir( $this->upload_dir, 0755 );
+			} else {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	public function filter_plugin_config($actions, $plugin_id) {
+		if ($plugin_id == $this->plugin_id()){
+			$actions[] = 'Configure';
+			$actions[] = 'Reset Configuration';
+		}
+
+		return $actions;
+	}
+
+	public function action_plugin_ui($plugin_id, $action) {
+		if ($plugin_id == $this->plugin_id()){
+			switch ($action){
+				case 'Reset Configuration':
+					$this->default_paths(true);
+					echo _t("Upload directory has been set to use Habari Media Silo's.");
+					break;
+				case 'Configure' :
+					$ui = new FormUI( strtolower( get_class( $this ) ) );
+					$upload_dir= $ui->append( 'text', 'metaweblog_upload_dir', 'option:metaweblog__upload_dir', _t( 'Directory to use when uploading new media:' ) );
+					$ui->append( 'text', 'metaweblog_upload_url', 'option:metaweblog__upload_url', _t( 'URL to upload directory:' ) );
+					$upload_dir->add_validator( array( $this, 'validate_upload_dir' ) );
+					$ui->append( 'submit', 'save', _t( 'Save' ) );
+					$ui->set_option( 'success_message', _t('Options saved') );
+					$ui->out();
+					break;
+			}
+		}
+	}
+	
+	// Need to use check_files somehow
+	public function validate_upload_dir( $path ) {
+		if ( !is_dir( $path ) ) {
+			if ( is_writable( dirname( $path ) ) ) {
+				mkdir( $path, 0755 );
+			} else {
+				return array( _t('Failed to create the upload directory for metaWeblog.') );
+			}
+		}
+		
+		return array();
 	}
 	
 	public function filter_rsd_api_list( $list ) {
@@ -25,15 +138,6 @@ class metaWeblog extends Plugin
 		);
 		$list['Atom']['preferred']= 'false';
 		return $list;
-	}
-	
-	public function action_xmlrpcexception_get_messages( $code ) {
-		switch ( $code ) {
-			case 801:
-				return _t( 'Login Error (probably bad username/password combination)' );
-			case 806:
-				return _t( 'No Such Item' );
-		}
 	}
 	
 	public function is_auth( $username, $password, $force = FALSE ) {
@@ -171,6 +275,34 @@ class metaWeblog extends Plugin
 		$struct = new XMLRPCStruct();
 		$struct->postid = $post->id;
 
+		return $struct;
+	}
+	
+	/**
+	 * Needs work, validate media type, max file size
+	 */
+	public function xmlrpc_metaWeblog__newMediaObject( $params ) {
+		$user = $this->is_auth( $params[1], $params[2] );
+		
+		$name = strtolower(basename($params[3]->name));
+		
+		do {
+			if ($dot = strrpos( $name, '.' )) {
+				$filename = substr_replace( $name, '_' . date('omdhis'), $dot, 0 );
+			}
+			else {
+				$filename = $name . '_' . date('omdhis');
+			}
+		} while (file_exists($filename));
+		
+		if ( !file_put_contents(rtrim($this->upload_dir,'\/') . '/' . $filename_new, base64_decode($params[3]->bits), LOCK_EX) ) {
+			$exception = new XMLRPCException(810);
+			$exception->output_fault_xml();
+		}
+		
+		$struct = new XMLRPCStruct();
+		$struct->url = rtrim($this->upload_url,'\/') . '/' . $filename;
+		
 		return $struct;
 	}
 
